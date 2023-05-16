bf00d15e9a7465839f9414d3fde1c17cfbbaa352
Ivan (John) Kosiakov <92152254+RAYNingTime@users.noreply.github.com>
2023-05-16 23:34:41 +0300

    Create model_training.py

---
+++ useful_templates/model_training.py
@@ -0,0 +1,7 @@
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+
+def train_model(X_train, y_train):
+    model = RandomForestClassifier()
+    model.fit(X_train, y_train)
+    return model
