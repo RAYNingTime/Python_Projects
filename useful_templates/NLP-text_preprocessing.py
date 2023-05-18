18b9de777493cd43c9f6b6dad483b730cebd9122
Ivan (John) Kosiakov <92152254+RAYNingTime@users.noreply.github.com>
2023-05-18 22:23:31 +0300

    Create NLP-text_preprocessing.py

---
+++ useful_templates/NLP-text_preprocessing.py
@@ -0,0 +1,22 @@
+import re
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+def preprocess_text(text):
+    # Remove special characters and numbers
+    text = re.sub(r'[^a-zA-Z]', ' ', text)
+    
+    # Convert to lowercase
+    text = text.lower()
+    
+    # Tokenize text into words
+    words = word_tokenize(text)
+    
+    # Remove stop words
+    stop_words = set(stopwords.words('english'))
+    words = [word for word in words if word not in stop_words]
+    
+    # Join the words back into a single string
+    processed_text = ' '.join(words)
+    
+    return processed_text
