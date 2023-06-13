d5c5082a347a1bcffd71bcd39bfef6a4d41de8b8
Ivan (John) Kosiakov <92152254+RAYNingTime@users.noreply.github.com>
2023-06-13 15:39:44 +0300

    Update cs231n_py_basic.py

--- cs231n_py_basic.py
+++ cs231n_py_basic.py
@@ -307,3 +307,31 @@ print(a[[0, 0], [1, 1]])  # Prints "[2 2]"
 
 # Equivalent to the previous integer array indexing example
 print(np.array([a[0, 1], a[0, 1]]))  # Prints "[2 2]"
+
+
+# One useful trick with integer array indexing is selecting or mutating one element from each row of a matrix:
+#  |  numpy.arange([start, ]stop, [step, ]dtype=None, *, like=None) |
+#  |  Return evenly spaced values within a given interval.          |
+#  |  Example: np.arrange(3)   |   Prints: array([0, 1, 2])         |
+
+# Create a new array from which we will select elements
+a = np.array([[1,2,3], [4,5,6], [7,8,9], [10, 11, 12]])
+
+print(a)  # prints "array([[ 1,  2,  3],
+          #                [ 4,  5,  6],
+          #                [ 7,  8,  9],
+          #                [10, 11, 12]])"
+
+# Create an array of indices
+b = np.array([0, 2, 0, 1])
+
+# Select one element from each row of a using the indices in b
+print(a[np.arange(4), b])  # Prints "[ 1  6  7 11]"
+
+# Mutate one element from each row of a using the indices in b
+a[np.arange(4), b] += 10
+
+print(a)  # prints "array([[11,  2,  3],
+          #                [ 4,  5, 16],
+          #                [17,  8,  9],
+          #                [10, 21, 12]])
