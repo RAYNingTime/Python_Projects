182724e02fa1b2e8d19b812217703687c12b3f73
Ivan (John) Kosiakov <92152254+RAYNingTime@users.noreply.github.com>
2023-06-15 06:56:32 +0300

    Update cs231n_py_basic.py

--- cs231n_py_basic.py
+++ cs231n_py_basic.py
@@ -335,5 +335,37 @@ print(a)  # prints "array([[11,  2,  3],
           #                [ 4,  5, 16],
           #                [17,  8,  9],
           #                [10, 21, 12]])
-      
+
+
+# Boolean array indexing
+a = np.array([[1,2], [3, 4], [5, 6]])
+
+bool_idx = (a > 2)   # Find the elements of a that are bigger than 2;
+                     # this returns a numpy array of Booleans of the same
+                     # shape as a, where each slot of bool_idx tells
+                     # whether that element of a is > 2.
+
+print(bool_idx)      # Prints "[[False False]
+                     #          [ True  True]
+                     #          [ True  True]]"
+
+# We use boolean array indexing to construct a rank 1 array
+# consisting of the elements of a corresponding to the True values
+# of bool_idx
+print(a[bool_idx])  # Prints "[3 4 5 6]"
+
+# We can do all of the above in a single concise statement:
+print(a[a > 2])     # Prints "[3 4 5 6]"
+
+# Datatypes
+x = np.array([1, 2])   # Let numpy choose the datatype
+print(x.dtype)         # Prints "int64"
+
+x = np.array([1.0, 2.0])   # Let numpy choose the datatype
+print(x.dtype)             # Prints "float64"
+
+x = np.array([1, 2], dtype=np.int64)   # Force a particular datatype
+print(x.dtype)       
+
+
       
