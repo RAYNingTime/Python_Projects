d2a3384e3fe4d492548e562f3fa6be7de1208e9b
Ivan (John) Kosiakov <92152254+RAYNingTime@users.noreply.github.com>
2023-06-15 07:00:08 +0300

    Update cs231n_py_basic.py

--- cs231n_py_basic.py
+++ cs231n_py_basic.py
@@ -421,4 +421,22 @@ print(np.dot(x, v))
 print(x.dot(y))
 print(np.dot(x, y))
 
+# SPecial sign .sum 
+x = np.array([[1,2],[3,4]])
+
+print(np.sum(x))  # Compute sum of all elements; prints "10"
+print(np.sum(x, axis=0))  # Compute sum of each column; prints "[4 6]"
+print(np.sum(x, axis=1))  # Compute sum of each row; prints "[3 7]"
+
+# Transpose a matrix, simply use the T attribute of an array object
+x = np.array([[1,2], [3,4]])
+print(x)    # Prints "[[1 2]
+            #          [3 4]]"
+print(x.T)  # Prints "[[1 3]
+            #          [2 4]]"
+
+# Note that taking the transpose of a rank 1 array does nothing:
+v = np.array([1,2,3])
+print(v)    # Prints "[1 2 3]"
+print(v.T)  # Prints "[1 2 3]"
       
